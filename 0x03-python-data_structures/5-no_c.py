c456943cf2861a240b6dab91ef34d1d122b7f5ec
Daniel Yohannes <leinad.sennahoy@gmail.com>
2024-04-11 04:27:30 +0300

    0x03

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author: Daniel Yohannes
+
+
+def no_c(my_string):
+    """Remove all characters c and C from a string."""
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
